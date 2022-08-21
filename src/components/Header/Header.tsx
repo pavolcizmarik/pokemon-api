c84f84f88b620e91484087a3c0312abef234bfa2
cizma <pavol.cizmarik@hotmail.sk>
2022-08-21 21:03:50 +0200

    change logo to static image instead of external reference

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 
-import { EXT_IMG } from "../../constants";
+import logo from "../../assets/logo.png";
 
 export const Header: React.FC = () => {
   return (
     <>
       <div className="w-full pl-4 pr-4 bg-red-600">
         <div className="max-w-7xl m-auto">
-          <img src={EXT_IMG.LOGO} alt="logo" className="d-block w-40 py-4" />
+          <img src={logo} alt="logo" className="d-block w-40 py-4" />
         </div>
       </div>
     </>
