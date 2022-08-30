f42b41eea1f088efd00c2a288e57a1808714ce20
pavolcizmarik <pavol.cizmarik@hotmail.sk>
2022-08-30 12:19:17 +0200

    removed comments

--- src/components/PokemonList/PokemonList.tsx
+++ src/components/PokemonList/PokemonList.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 
 import axios from "axios";
 import { API } from "../../constants";
-import { /* IFetchPokemonResponse, */ IPokemonBasic } from "../../types";
+import { IPokemonBasic } from "../../types";
 
 import { PokemonCard } from "../PokemonCard";
 import { LoadMore } from "./PokemonList.styled";
@@ -10,7 +10,6 @@ import { Container, ContainerWrapper } from "../Layout/Layout.styled";
 import { Loader } from "../Loader";
 
 export const PokemonList: React.FC = () => {
-  // const [data, setData] = useState<IFetchPokemonResponse[]>([]);
   const [pokemons, setPokemons] = useState<IPokemonBasic[]>([]);
   const [loading, setLoading] = useState(true);
   const [requestUrl, setRequestUrl] = useState(API.GET_POKEMONS);
@@ -20,7 +19,6 @@ export const PokemonList: React.FC = () => {
     try {
       const result = await axios.get(requestUrl);
 
-      // setData(result.data as IFetchPokemonResponse[]);
       setPokemons([...pokemons, ...result.data.results]);
       setRequestUrl(result.data.next);
       setLoading(false);
