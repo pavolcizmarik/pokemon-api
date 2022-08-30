8d96ff814ea5219530c997df3dd1dfea35106e2b
pavolcizmarik <pavol.cizmarik@hotmail.sk>
2022-08-30 12:21:25 +0200

    change logo as styled component

---
+++ src/components/Header/Header.styled.ts
@@ -0,0 +1,7 @@
+import tw from "tailwind-styled-components";
+
+export const Logo = tw.img`
+  d-block 
+  w-40 
+  py-4
+`;
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,13 +2,14 @@ import React from "react";
 
 import logo from "../../assets/logo.png";
 import { Container, ContainerWrapper } from "../Layout/Layout.styled";
+import { Logo } from "./Header.styled";
 
 export const Header: React.FC = () => {
   return (
     <>
       <Container className="bg-red-600">
         <ContainerWrapper>
-          <img src={logo} alt="logo" className="d-block w-40 py-4" />
+          <Logo src={logo} alt="logo" />
         </ContainerWrapper>
       </Container>
     </>
